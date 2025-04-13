f07685f49cee8125038b8b616b006155433fc74b
Davi <andrielysom@gmail.com>
2025-04-13 19:37:57 -0300

    Vai

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,24 @@
 let author = '';
 
-function toggleBoxForNewUser(met) {
-    if (met === 'tog') {
-        let input = document.getElementById('enter-user');
-        input.classList.toggle('active');
-        input.focus();
-    } else if (met === 'get') {
-        const userType = document.querySelector('input[name=userType]:checked').value;
-        let userName = document.getElementById('input-name').value;
-        let userDetail = document.getElementById('input-detail').value;
+function submitLogin() {
+    const userType = document.querySelector('input[name=userType]:checked').value;
+    let userName = document.getElementById('input-name').value;
+    let userDetail = document.getElementById('input-detail').value;
 
-        if (userName.length < 4 || userDetail.length < 3) {
-            alert('Erro: Nome ou detalhe muito curto.');
-            return null;
-        }
-
-        const formattedUser = userType === 'Cliente'
-            ? `${userName} | ${userDetail}`
-            : `${userName} | ${userDetail}`;
-        localStorage.setItem('user', formattedUser);
-        author = formattedUser;
-
-        toggleBoxForNewUser('tog');
+    if (userName.length < 4 || userDetail.length < 3) {
+        alert('Erro: Nome ou detalhe muito curto.');
+        return null;
     }
+
+    const formattedUser = userType === 'Cliente'
+        ? `${userName} | ${userDetail}`
+        : `${userName} | ${userDetail}`;
+    localStorage.setItem('user', formattedUser);
+    author = formattedUser;
+
+    // Oculta a página de login e mostra o chat
+    document.getElementById('login-page').classList.add('hidden');
+    document.getElementById('chat-page').classList.remove('hidden');
 }
 
 function updateFields() {
@@ -33,18 +29,5 @@ function updateFields() {
         detailField.placeholder = 'Bairro';
     } else if (userType === 'Profissional') {
         detailField.placeholder = 'Profissão';
-    }
-}
-
-function Submit(event) {
-    event.preventDefault();
-    const message = document.querySelector('#input-message').value;
-    const now = new Date();
-    const time = `${now.getHours()}:${now.getMinutes()}`;
-
-    if (message.length) {
-        const messageObject = { author, message, time };
-        renderMessage(messageObject);
-        socket.emit('sendMessage', messageObject);
     }
 }
