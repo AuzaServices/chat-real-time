7d3cce3db4826a9c02d87cb39f76cab536b8f2f9
Davi <andrielysom@gmail.com>
2025-04-14 20:09:44 -0300

    vai

--- server.js
+++ server.js
@@ -1,58 +1,42 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const compression = require('compression'); // Importando o middleware de compressão
-const morgan = require('morgan'); // Importando o morgan para logs
+const express = require( 'express' )
+const app = express()
+const path = require( 'path' )
 
-const { join } = path;
+const { join } = path
 
-const port = process.env.PORT || 4000;
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-  cors: {
-    origin: "*"
-  },
-  perMessageDeflate: {
-    threshold: 1024, // Comprime apenas mensagens acima de 1KB
-  }
-});
+const port = process.env.PORT || 4000
+const server = require( 'http' ).createServer( app )
+const io = require( 'socket.io' )( server )
 
-app.use(compression()); // Habilitando a compressão
-app.use(morgan('dev')); // Habilitando logs com morgan
-app.use(express.static(join(__dirname, 'public')));
-app.set('views', join(__dirname, 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.use( express.static( join( __dirname , 'public' ) ) )
+app.set( 'views' , join( __dirname , 'public' ) )
+app.engine( 'html' , require('ejs').renderFile )
+app.set( 'view engine' , 'html' )
 
-app.use('/', (req, res) => {
-  res.render('index.html');
-});
+app.use( '/' , ( req , res ) => {
+  res.render( 'index.html' )
+} )
 
-let messages = [];
+let messages = []
 let connectionsInfo = {
   connections: 0
-};
+}
 
-io.on('connection', socket => {
-  server.getConnections((err, count) => {
-    if (err) throw err;
-    connectionsInfo.connections = count;
-    socket.emit('ConnectionsInfo', connectionsInfo);
-  });
+io.on( 'connection' , socket => {
 
-  socket.emit('previousMessages', messages);
+  connectionsInfo.connections = server.getConnections( ( err , count ) => {
+    return count
+  } )
+  
+  socket.emit( 'ConnectionsInfo' , connectionsInfo )
+  socket.emit( 'previousMessages' , messages )
 
-  socket.on('sendMessage', data => {
-    messages.push(data);
-    socket.broadcast.emit('receivedMessage', data);
-  });
+  socket.on( 'sendMessage' , data => {
+    messages.push( data )
+    socket.broadcast.emit( 'receivedMessage' , data )
+  } )
+} )
 
-  socket.on('clearMessages', () => {
-    messages = [];
-    io.emit('clearMessages');
-  });
-});
-
-server.listen(port, () => {
-  console.log(`Server running on localhost:${port}`);
-});
+server.listen( port , () => {
+  console.log( `Server running on localhost:${port}` )
+} )
