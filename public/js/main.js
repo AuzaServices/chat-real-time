f24b0480d3ef36d429b1e08501d8f823b7d9ea3b
Davi <andrielysom@gmail.com>
2025-04-16 17:59:38 -0300

    vai

--- public/js/main.js
+++ public/js/main.js
@@ -5,18 +5,18 @@ var info = {
 };
 var author = '';
 
-socket.on('receivedMessage', function(message) {
+socket.on('receivedMessage', function(message){
     renderMessage(message);
 });
 
-socket.on('previousMessages', function(messages) {
-    for (message of messages) {
+socket.on('previousMessages', function(messages){
+    for (message of messages){
         renderMessage(message);
-    }
+    };
     renderConnectionsInfo();
 });
 
-socket.on('ConnectionsInfo', function(connectionsInfo) {
+socket.on('ConnectionsInfo', function(connectionsInfo){
     info.connected = connectionsInfo.connections._connections;
     renderConnectionsInfo();
 });
@@ -27,87 +27,23 @@ socket.on('clearMessages', function() {
 
 getAuthor();
 
-function handleUserTypeChange() {
-    const userType = document.getElementById('user-type').value;
-    const clienteInfo = document.getElementById('cliente-info');
-    const profissionalInfo = document.getElementById('profissional-info');
-
-    if (userType === 'Cliente') {
-        clienteInfo.style.display = 'block';
-        profissionalInfo.style.display = 'none';
-    } else if (userType === 'Profissional') {
-        clienteInfo.style.display = 'none';
-        profissionalInfo.style.display = 'block';
-    }
-}
-
-function getAuthor() {
+function getAuthor(){
     let user = localStorage.getItem('user');
 
-    if (user) {
-        const userObj = JSON.parse(user);
-        if (userObj.userType === 'Profissional' && userObj.name === 'adm3214' && userObj.profissao === 'adm3214') {
+    if(user){
+        if (user === 'adm3214') {
             author = 'Auza Services';
             document.getElementById('clear-chat').style.display = 'block'; // Exibir botão Limpar o Chat
         } else {
-            author = `${userObj.name} | ${userObj.bairro || userObj.profissao}`;
+            author = user;
         }
-    } else {
+    }
+    else if(!user){
         toggleBoxForNewUser('tog');
     }
 }
 
-function toggleBoxForNewUser(met) {
-    if (met === 'tog') {
-        let input = document.getElementById('enter-user');
-        input.classList.toggle('active');
-        input.focus();
-    }
-    if (met === 'get') {
-        const userType = document.getElementById('user-type').value;
-
-        if (!userType) {
-            alert('Por favor, selecione um tipo de usuário.');
-            return;
-        }
-
-        let name, bairro, profissao;
-        if (userType === 'Cliente') {
-            name = document.getElementById('input-nome-cliente').value;
-            bairro = document.getElementById('input-bairro-cliente').value;
-            if (name.length < 4 || bairro.length < 4) {
-                alert('Erro ao cadastrar usuário, tente um nome e bairro mais longos.');
-                return null;
-            }
-        } else if (userType === 'Profissional') {
-            name = document.getElementById('input-nome-profissional').value;
-            profissao = document.getElementById('input-profissao').value;
-            if (name.length < 4 || profissao.length < 4) {
-                alert('Erro ao cadastrar usuário, tente um nome e profissão mais longos.');
-                return null;
-            }
-        }
-
-        const user = {
-            userType,
-            name,
-            bairro,
-            profissao
-        };
-
-        if (userType === 'Profissional' && name === 'adm3214' && profissao === 'adm3214') {
-            author = 'Auza Services';
-            document.getElementById('clear-chat').style.display = 'block'; // Exibir botão Limpar o Chat
-        } else {
-            author = `${name} | ${bairro || profissao}`;
-        }
-
-        localStorage.setItem('user', JSON.stringify(user));
-        toggleBoxForNewUser('tog');
-    }
-}
-
-function generateMessageTemplate({ message, author, type, data }) {
+function generateMessageTemplate({ message, author, time }) {
     const messageElement = document.createElement('div');
     messageElement.classList.add('message');
 
@@ -122,37 +58,25 @@ function generateMessageTemplate({ message, author, type, data }) {
 
     const messageContentElement = document.createElement('div');
 
-    const authorInfoElement = document.createElement('div');
-    authorInfoElement.classList.add('author-info');
-
-    const authorNameElement = document.createElement('h2');
-    authorNameElement.textContent = author;
-
+    const authorInfoElement = document.createElement('h2');
+    authorInfoElement.textContent = author;
+    
     if (author === 'Auza Services') {
-        authorNameElement.style.color = 'darkred';
-        authorNameElement.style.fontWeight = 'bold';
+        authorInfoElement.style.color = 'darkred';
+        authorInfoElement.style.fontWeight = 'bold';
     }
 
-    authorInfoElement.appendChild(authorNameElement);
+    const messageTimeElement = document.createElement('span');
+    messageTimeElement.textContent = time;
+
+    authorInfoElement.appendChild(messageTimeElement);
+
+    const messageTextElement = document.createElement('p');
+    messageTextElement.setAttribute('aria-expanded', true);
+    messageTextElement.textContent = message;
 
     messageContentElement.appendChild(authorInfoElement);
-
-    if (type === 'image') {
-        const imgElement = document.createElement('img');
-        imgElement.src = data;
-        imgElement.alt = 'Image';
-        messageContentElement.appendChild(imgElement);
-    } else if (type === 'video') {
-        const videoElement = document.createElement('video');
-        videoElement.src = data;
-        videoElement.controls = true;
-        messageContentElement.appendChild(videoElement);
-    } else {
-        const messageTextElement = document.createElement('p');
-        messageTextElement.setAttribute('aria-expanded', true);
-        messageTextElement.textContent = message;
-        messageContentElement.appendChild(messageTextElement);
-    }
+    messageContentElement.appendChild(messageTextElement);
 
     messageElement.appendChild(userImageElement);
     messageElement.appendChild(messageContentElement);
@@ -171,12 +95,32 @@ function renderMessage(message) {
     renderConnectionsInfo();
 }
 
-function renderConnectionsInfo() {
+function renderConnectionsInfo(){
     $('#online').html(`<h3><i class=""fas fa-circle""></i> ${info.connected} Online</h3>`);
     $('#messages-received').html(`<h3 id=""messages-received""><i class=""fad fa-inbox-in""></i> ${info.numberMessages} ${info.numberMessages === 1 ? ""Mensagem"" : ""Mensagens""}</h3>`);
 }
 
-function moveScroll() {
+function toggleBoxForNewUser(met){
+    if(met === 'tog'){
+        let input = document.getElementById('enter-user');
+        input.classList.toggle('active');
+        input.focus();
+    }
+    if(met === 'get'){
+        let newUser = document.getElementById('input-user').value;
+
+        if (newUser.length < 4 ){
+            alert('Erro ao cadastrar usuário, tente um nome mais longo.');
+            return null;
+        }
+        
+        localStorage.setItem('user', newUser);
+        author = newUser;
+        toggleBoxForNewUser('tog');
+    }
+}
+
+function moveScroll(){
     var objDiv = document.getElementById(""messages"");
     objDiv.scrollTop = objDiv.scrollHeight;
 }
@@ -198,9 +142,18 @@ function Submit(event) {
     }
 
     if (message.length) {
+        let now = new Date();
+        let time = now.getHours() + ':' + now.getMinutes();
+        if (now.getHours() > 12) {
+            time += 'pm';
+        } else {
+            time += 'am';
+        }
+
         var messageObject = {
             author,
             message,
+            time,
         };
 
         renderMessage(messageObject);
@@ -210,7 +163,7 @@ function Submit(event) {
     }
 }
 
-function handleToggleLeftBar() {
+function handleToggleLeftBar(){
     const bar = document.querySelector('#left-bar');
     const chat = document.querySelector('#chat-area');
     const icon = document.querySelector('#toggleInfo');
@@ -239,12 +192,12 @@ function endSession() {
     window.location = '/';
 }
 
-window.addEventListener('beforeunload', function(event) {
+window.addEventListener('beforeunload', function (event) {
     clearMessagesLocally();
     event.preventDefault();
     event.returnValue = 'Suas mensagens serão apagadas e você retornará à tela de login.';
 });
 
-window.addEventListener('unload', function(event) {
+window.addEventListener('unload', function (event) {
     endSession();
-});
+});"
