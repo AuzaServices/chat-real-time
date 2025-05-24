e946afdb7c4ee0a6f957110a2255981bf8937a87
Davi <andrielysom@gmail.com>
2025-05-23 23:26:21 -0300

    vai

--- public/js/profissional.js
+++ public/js/profissional.js
@@ -8,16 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const professional = JSON.parse(professionalData);
 
-    const professionalCard = `
+    document.getElementById("professional-card").innerHTML = `
         <div class="card">
             <h3>${professional.name}</h3>
             <p>${professional.city}</p>
-            <p>Idade: ${professional.age} anos</p>
-            <p>Avaliação: ${professional.stars}</p>
             <p>${professional.comment}</p>
             <a href="https://api.whatsapp.com/send?phone=${professional.whatsapp}&text=Olá, gostaria de informações sobre seus serviços." target="_blank">Contato via WhatsApp</a>
         </div>
     `;
-
-    document.getElementById("professional-card").innerHTML = professionalCard;
 });
