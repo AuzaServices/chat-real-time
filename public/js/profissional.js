86254698f2e9c70bec60664f46603a08e207f97d
Davi <andrielysom@gmail.com>
2025-05-24 17:21:17 -0300

    vai

--- public/js/profissional.js
+++ public/js/profissional.js
@@ -45,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const professionalUrl = window.location.href;
 
+document.querySelector('meta[property="og:url"]').setAttribute("content", professionalUrl);
 document.querySelector('meta[property="og:title"]').setAttribute("content", `${professional.name} - Profissional`);
 document.querySelector('meta[property="og:description"]').setAttribute("content", professional.comment);
 document.querySelector('meta[property="og:url"]').setAttribute("content", professionalUrl);
