e96718a5fae037d48244545a1bce690a8f9756d6
Davi <andrielysom@gmail.com>
2025-05-25 15:37:41 -0300

    vai

--- public/js/splash.js
+++ public/js/splash.js
@@ -3,3 +3,23 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = "index.html"; // Após o splash, redireciona para index.html
     }, 3000); // Tempo de exibição do splash antes do redirecionamento
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sloganElement = document.getElementById("slogan-text");
+    const sloganText = "Tudo em serviço de A a Z";
+    let index = 0;
+
+    function typeEffect() {
+        if (index < sloganText.length) {
+            sloganElement.innerHTML += sloganText.charAt(index);
+            index++;
+            setTimeout(typeEffect, 100); // Ajuste o tempo para controlar a velocidade da digitação
+        }
+    }
+
+    typeEffect();
+
+    setTimeout(() => {
+        window.location.href = "index.html"; // Redireciona após exibir o slogan
+    }, 4000); // Tempo total do efeito antes do redirecionamento
+});
