86a6cdd60b552de791d53ad6ffe81cefca2b19b1
Davi <andrielysom@gmail.com>
2025-05-25 12:03:43 -0300

    vai

--- public/js/splash.js
+++ public/js/splash.js
@@ -1,11 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const splashLogo = document.querySelector(".splash-container");
-
     setTimeout(() => {
-        splashLogo.classList.add("fade-out");
-
-        setTimeout(() => {
-            window.location.replace("index.html"); // Redireciona corretamente
-        }, 2000);
-    }, 3000);
+        window.location.href = "index.html"; // Redireciona após o splash
+    }, 3000); // Tempo que a splash deve ser exibida antes do redirecionamento
 });
