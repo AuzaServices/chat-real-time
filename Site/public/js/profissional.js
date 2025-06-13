48d25f32a67b8e324d74b2936580493c837715f1
Davi <andrielysom@gmail.com>
2025-06-13 17:12:40 -0300

    vai

--- Site/public/js/profissional.js
+++ Site/public/js/profissional.js
@@ -236,7 +236,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Lista de profissionais destacados
     const highlightedProfessionals = new Set([
-        "Edmilson Camara","Mateus Santos", "Bruna Costa", "Carlos Costa", "Ana Souza", "Lucas Oliveira", "André Souza",
+        "Edimilson Camara","Mateus Santos", "Bruna Costa", "Carlos Costa", "Ana Souza", "Lucas Oliveira", "André Souza",
         "Diego Rocha", "Marcos Vinicius", "Bruno Ferreira", "Carlos Mendes", "Eduarda Nunes",
         "Fernanda Ramos", "Gustavo Ramos", "Diego Martins", "Carlos Nogueira", "José Lima"
     ]);
