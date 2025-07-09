ecf9a1ed9f3c86ad14c6c24c8297ca926ac788f9
Davi <andrielysom@gmail.com>
2025-07-09 17:16:27 -0300

    vai

--- Site/public/js/profissional.js
+++ Site/public/js/profissional.js
@@ -102,7 +102,7 @@ document.addEventListener("DOMContentLoaded", function () {
         { id: 65, service: "Montador de Móveis", name: "Bergson Nunes", age: 36, city: "Horizonte - CE", stars: "⭐⭐⭐⭐⭐", comment: "18 anos de experiência. Atendo em Fortaleza e Regiões metropolitanas", whatsapp: "5585994316980" }, 
         { id: 63, service: "Montador de Móveis", name: "Felipe Araújo", age: 30, city: "Horizonte - CE", stars: "⭐⭐⭐", comment: "Montagens em Geral.", whatsapp: "5588920007471" },
         { id: 64, service: "Montador de Móveis", name: "Robson", age: 27, city: "Horizonte - CE", stars: "⭐⭐⭐", comment: "9 anos de Experiência na área.", whatsapp: "5585994050965" },
-        { id: 66, service: "Montador de Móveis", name: "Joilson Nunes", age: 40, city: "Caucaia - CE", stars: "⭐⭐⭐", comment: "Experiência com Montagem de Móveis", whatsapp: "5585988741249" },
+        { id: 66, service: "Montador de Móveis", name: "Joilson Nunes", age: 40, city: "Caucaia - CE", stars: "⭐⭐⭐", comment: "Experiência com Montagem de Móveis", whatsapp: "5585988741249" , imagens: [ "https://i.imgur.com/NgHHEKd.jpeg", "https://i.imgur.com/uAr2BvV.jpeg", "https://i.imgur.com/pzN0aAl.png", "https://i.imgur.com/2EYNkdl.png" ]},
         //
         { id: 67, service: "Montador de Móveis", name: "Gustavo Ramos", age: 42, city: "Pacajus - CE", stars: "⭐⭐⭐⭐", comment: "Garantia de serviço bem executado.", whatsapp: "55859913407", imagens: [ "https://i.imgur.com/NgHHEKd.jpeg", "https://i.imgur.com/uAr2BvV.jpeg", "https://i.imgur.com/pzN0aAl.png", "https://i.imgur.com/2EYNkdl.png" ]},
         { id: 68, service: "Montador de Móveis", name: "Henrique Nunes", age: 36, city: "Horizonte - CE", stars: "⭐⭐⭐⭐⭐", comment: "Experiência com todos os tipos de móveis.", whatsapp: "55851340727" },
