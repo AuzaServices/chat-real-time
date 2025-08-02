539a8b53b1c1f274d2292e983ca0b7adbf1914dc
Davi <andrielysom@gmail.com>
2025-08-02 19:21:09 -0300

    vai

--- Site/server.js
+++ Site/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
 
 // Middleware
 app.use(express.static("public"));
